8e6c66b6c76d6f35ad3a01a64977e1d99f49b950
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2013-12-21 14:08:05 -0600

    add header and zellers

--- cal_month.rb
+++ cal_month.rb
@@ -1,8 +1,71 @@
 class Month
-
-  def self.zellers(day)
+  def initialize(month, year)
+    @month = month
+    @year = year
   end
 
+  def header
+    months = {1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July"}
+    string = "#{months[@month]} #{@year}".center(20).rstrip
+    string + "\nSu Mo Tu We Th Fr Sa"
+  end
+  def zeller
+    m = @month
+    y = @year
+    if m == 1 or m == 2
+      m += 12
+      y -= 1
+    end
+    day_one =(1 + (((m + 1) * 26) / 10) + y + (y / 4) + 6 * (y / 100) + (y / 400)) % 7
+  end
+
+# below is from interwebs
+  # def zeller(year, month, day)
+  #     months = %w[march april may june july august september october november december january february]
+  #     weekdays = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
+  #     k = day
+  #     m = months.index(month.downcase) + 1
+  #     y = (m <= 10) ? year : year-1
+  #     d = y % 100
+  #     c = y / 100
+  #     f = (k + (((13*m) - 1) / 5).floor + d + (d/4).floor + (c/4).floor - (2*c)) % 7
+  #     weekdays[f]
+  # end
+
+  # 1800.upto(3000) do |y|
+  #     puts "Five Friday, Saturday, and Sundays for October, #{y}" if zeller(y, "october", 1) == "Friday"
+  # end
 
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if @month == 1 || @month == 2
+    #   m = @month +12
+    #   y = @year - 1
+    # else
+    #   m = @month
+    #   y = @year
+    # end
--- test_cal.rb
+++ test_cal.rb
@@ -3,10 +3,48 @@ require 'minitest/autorun'
 
 class TestCalMonth < MiniTest::Unit::TestCase
 
-  def test_cal_month_has_zellers_method
-    Month.zellers("1")
+  def test_month_has_zellers_method
+    month = Month.new(1, 2000)
+    month.zeller
   end
 
+  def test_1_month_and_year_header
+    month = Month.new(1, 2012)
+    expected = <<EOS
+    January 2012
+Su Mo Tu We Th Fr Sa
+EOS
+  assert_equal(expected.chomp, month.header)
+  end
+
+  def test_2_month_and_year_header_feb
+    month = Month.new(2, 2000)
+    expected = <<EOS
+   February 2000
+Su Mo Tu We Th Fr Sa
+EOS
+  assert_equal(expected.chomp, month.header)
+  end
+
+  def test_3_zeller_on_monday
+    month = Month.new(5, 2000)
+    assert_equal(2, month.zeller)
+  end
+
+  def test_4_zeller_on_sun
+    month = Month.new(7, 2012)
+    assert_equal(1, month.zeller)
+  end
+
+  def test_5_zeller_on_jan
+    month = Month.new(1, 2000)
+    assert_equal(0, month.zeller)
+  end
+
+  def test_6_zeller_on_feb
+    month = Month.new(2, 2013)
+    assert_equal(6, month.zeller)
+  end
 
 
 end
