d9c6a4fee9e2fbc92ddd9dba35e1883dfcdd4d62
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2013-12-23 11:53:23 -0600

    array of days of the month

--- cal_month.rb
+++ cal_month.rb
@@ -5,7 +5,7 @@ class Month
   end
 
   def header
-    months = {1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July"}
+    months = {1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July", 8 => "August", 9 => "September", 10 => "October", 11 => "November", 12 => "December"}
     string = "#{months[@month]} #{@year}".center(20).rstrip
     string + "\nSu Mo Tu We Th Fr Sa"
   end
@@ -17,25 +17,21 @@ class Month
       y -= 1
     end
     day_one =(1 + (((m + 1) * 26) / 10) + y + (y / 4) + 6 * (y / 100) + (y / 400)) % 7
+    @day_one = day_one
   end
 
-# below is from interwebs
-  # def zeller(year, month, day)
-  #     months = %w[march april may june july august september october november december january february]
-  #     weekdays = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
-  #     k = day
-  #     m = months.index(month.downcase) + 1
-  #     y = (m <= 10) ? year : year-1
-  #     d = y % 100
-  #     c = y / 100
-  #     f = (k + (((13*m) - 1) / 5).floor + d + (d/4).floor + (c/4).floor - (2*c)) % 7
-  #     weekdays[f]
-  # end
+  def days_in_month
+    m = {1 => 31, 2 => 28, 3 => 31, 4 => 31, 5 => 31, 6 => 30, 7 => 31, 8 => 31, 9 => 30, 10 => 31, 11 => 30, 12 => 31}
+    days_in_month = "#{m[@month]}"
+    days_in_month.to_i
+    (1..(days_in_month.to_i)).to_a
+  end
 
-  # 1800.upto(3000) do |y|
-  #     puts "Five Friday, Saturday, and Sundays for October, #{y}" if zeller(y, "october", 1) == "Friday"
-  # end
 
+  def first_day_of_month
+    #{0 => "sa", 1 => "su", 2 => "mo", 3 => "tu" 4=> "we", 5 => "th", 6 => "fr"}
+    spaces = {1 => " ", 2 => "   ", 3 => "     "}
+  end
 
 end
 
--- test_cal.rb
+++ test_cal.rb
@@ -3,12 +3,12 @@ require 'minitest/autorun'
 
 class TestCalMonth < MiniTest::Unit::TestCase
 
-  def test_month_has_zellers_method
+  def test_1_month_has_zellers_method
     month = Month.new(1, 2000)
     month.zeller
   end
 
-  def test_1_month_and_year_header
+  def test_2_month_and_year_header
     month = Month.new(1, 2012)
     expected = <<EOS
     January 2012
@@ -17,7 +17,7 @@ EOS
   assert_equal(expected.chomp, month.header)
   end
 
-  def test_2_month_and_year_header_feb
+  def test_3_month_and_year_header_feb
     month = Month.new(2, 2000)
     expected = <<EOS
    February 2000
@@ -26,24 +26,49 @@ EOS
   assert_equal(expected.chomp, month.header)
   end
 
-  def test_3_zeller_on_monday
+  def test_4_zeller_on_monday
     month = Month.new(5, 2000)
     assert_equal(2, month.zeller)
   end
 
-  def test_4_zeller_on_sun
+  def test_5_zeller_on_sun
     month = Month.new(7, 2012)
     assert_equal(1, month.zeller)
   end
 
-  def test_5_zeller_on_jan
+  def test_6_zeller_on_jan
     month = Month.new(1, 2000)
     assert_equal(0, month.zeller)
   end
 
-  def test_6_zeller_on_feb
+  def test_7_zeller_on_feb
     month = Month.new(2, 2013)
     assert_equal(6, month.zeller)
+  end
+
+  def test_8_method_days_in_month_exists
+    month = Month.new(1, 2000)
+    month.days_in_month
+  end
+
+  def test_9_days_in_month_on_jan
+    month = Month.new(1, 2000)
+    assert_equal(31, month.days_in_month)
+  end
+
+  def test_10_days_in_month_on_sept
+    month = Month.new(9, 2000)
+    assert_equal(30, month.days_in_month)
+  end
+
+  def test_11_first_day_of_month_exists
+    month = Month.new(9, 2000)
+    month.first_day_of_month
+  end
+
+  def test_12_first_day_of_month_on_sept
+    # month = Month.new(9, 2013)
+    # assert_equal(, month.first_day_of_month)
   end
 
 
