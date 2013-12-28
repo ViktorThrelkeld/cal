dc39fa33b7fb037f07b181b0ac7021807e9fb707
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2013-12-28 12:09:07 -0600

    day array spaces

--- cal_month.rb
+++ cal_month.rb
@@ -7,7 +7,7 @@ class Month
   def header
     months = {1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July", 8 => "August", 9 => "September", 10 => "October", 11 => "November", 12 => "December"}
     string = "#{months[@month]} #{@year}".center(20).rstrip
-    @header = string + "\nSu Mo Tu We Th Fr Sa"
+    string + "\nSu Mo Tu We Th Fr Sa"
   end
 
   def zeller
@@ -18,40 +18,35 @@ class Month
       y -= 1
     end
     day_one =(1 + (((m + 1) * 26) / 10) + y + (y / 4) + 6 * (y / 100) + (y / 400)) % 7
-    @day_one = day_one
+    day_one
   end
 
   def days_in_month
-    m = {1 => 31, 2 => 28, 3 => 31, 4 => 31, 5 => 31, 6 => 30, 7 => 31, 8 => 31, 9 => 30, 10 => 31, 11 => 30, 12 => 31}
-    days_in_month = "#{m[@month]}"
-    d = days_in_month.to_i
-    # a = (1..d).to_a
-    # r = (1..d).to_s
-    r = (1..d)
-
-    # a[0..d].each {|day| print day}
-    @days_in_month = r
-
+    if @month == 2 and (@year % 400 == 0 or (@year % 4 == 0 and @year % 100 != 0))
+      (1..29).to_a
+    else
+      m = {1 => 31, 2 => 28, 3 => 31, 4 => 31, 5 => 31, 6 => 30, 7 => 31, 8 => 31, 9 => 30, 10 => 31, 11 => 30, 12 => 31}
+      days = m[@month]
+      r = (1..days).to_a
+    end
   end
 
   def first_day_of_month
-    spaces = {1 => " ", 2 => "    ", 3 => "       ", 4 => "          ", 5 => "             ", 6 => "                ", 0 => "                   "}
+    spaces = {1 => "", 2 => "   ", 3 => "      ", 4 => "         ", 5 => "            ", 6 => "               ", 0 => "                  "}
     zeller
-    @day_starts = "#{spaces[@day_one]}"
+    spaces[zeller]
   end
 
   def spacing
-    days_in_month
-    header
-    first_day_of_month
-    # days_in_month.to_i
-    # @days_in_month.split(", ")
-    # spacing = "#{@days_in_month}"
-    @days_in_month.to_a
-    days = @days_in_month.map{|d| Integer d}
-    # @days_in_month.each{|d| print d}
-    @header + "\n" + @day_starts + "#{days}"
-
+    days = days_in_month
+    days = days.map do |day|
+      if day < 10
+        " " + day.to_s
+      else
+        day
+      end
+    end
+    days = days.join(" ")
   end
 
 
--- test_cal.rb
+++ test_cal.rb
@@ -51,21 +51,41 @@ EOS
     month.days_in_month
   end
 
-#   def test_9_days_in_month_on_jan
-#     month = Month.new(1, 2000)
-#     expected = <<EOS
-# 1..31
-# EOS
-#     assert_equal(expected.chomp, month.days_in_month)
-#   end
+  def test_9_days_in_month_on_jan
+    month = Month.new(1, 2000)
+    expected = (1..31).to_a
+    assert_equal(expected, month.days_in_month)
+  end
 
-#   def test_10_days_in_month_on_sept
-#     month = Month.new(9, 2000)
-#     expected = <<EOS
-# 1..30
-# EOS
-#     assert_equal(expected.chomp, month.days_in_month)
-#   end
+  def test_10_days_in_month_on_sept
+    month = Month.new(9, 2000)
+    expected = (1..30).to_a
+    assert_equal(expected, month.days_in_month)
+  end
+
+  def test_10a_days_in_month_feb_non_leap_year
+    month = Month.new(2, 1991)
+    expected = (1..28).to_a
+    assert_equal(expected, month.days_in_month)
+  end
+
+  def test_10b_days_in_month_feb_leap_year
+    month = Month.new(2, 1996)
+    expected = (1..29).to_a
+    assert_equal(expected, month.days_in_month)
+  end
+
+  def test_10c_days_in_month_feb_leap_century
+    month = Month.new(2, 2000)
+    expected = (1..29).to_a
+    assert_equal(expected, month.days_in_month)
+  end
+
+  def test_10d_days_in_month_feb_non_leap_century
+    month =Month.new(2, 1900)
+    expected = (1..28).to_a
+    assert_equal(expected, month.days_in_month)
+  end
 
   def test_11_first_day_of_month_exists
     month = Month.new(9, 2000)
@@ -74,12 +94,12 @@ EOS
 
   def test_12_first_day_of_month_on_su
     month = Month.new(9, 2013)
-    assert_equal(" ", month.first_day_of_month)
+    assert_equal("", month.first_day_of_month)
   end
 
   def test_13_first_day_of_month_on_sa
     month = Month.new(6, 2013)
-    assert_equal("                   ", month.first_day_of_month)
+    assert_equal("                  ", month.first_day_of_month)
   end
 
   def test_14_spacing_exists
@@ -89,7 +109,8 @@ EOS
 
   def test_15_spacing_days_of_month
     month = Month.new(9, 2013)
-    assert_equal(" 1  2  3  4  5  6  7  8  9 10", month.spacing)
+    expected =" 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30"
+    assert_equal( expected, month.spacing )
   end
 
 end
