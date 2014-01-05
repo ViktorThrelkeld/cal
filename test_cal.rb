1fa2ed22eacf9dc946b6931fc936c13b4a5af0a5
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2014-01-05 11:37:38 -0600

    start integration testing

--- test_cal.rb
+++ test_cal.rb
@@ -4,3 +4,19 @@ require_relative 'cal'
 require 'minitest/autorun'
 
 class TestCal < MiniTest::Unit::TestCase
+
+  def test_1_get_month_i_was_born
+    expected = `cal 1 1977`
+    shell_output = `ruby cal.rb 1 1977`
+    assert_equal(expected, shell_output )
+  end
+
+  # def test_2_get_year_i_was_born
+  #   expected = `cal 1977`
+  #   assert_equal(expected, `ruby cal.rb 1977`)
+  # end
+
+
+
+
+end
